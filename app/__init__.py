5208557552224f3034d18a9237faf90bbc7d5b90
ConstanzeMaass2610 <constanze.maass@otto.de>
2020-07-08 12:00:09 +0200

    [frontend] refaktoring text data

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__, static_folder='static')
 
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import render_template
+import os
+
+from flask import render_template, send_from_directory
 from app import app
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'templates'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
