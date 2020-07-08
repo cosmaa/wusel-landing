1c11f83aa593ca8cee596e7d6dd4124370eda4c1
ConstanzeMaass2610 <constanze.maass@otto.de>
2020-07-08 08:57:26 +0200

    [frontend] vue frontend

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,7 @@
-from flask import render_template, flash, url_for, redirect
+from flask import render_template
 from app import app
 
 
 @app.route('/')
-def hello():
-    return render_template('pages/hello.html', title='space')
-
-
-@app.route('/impressum')
-def impressum():
-    return render_template('pages/impressum.html', title='impressum')
-
-
-@app.route("/wishes")
-def wishes():
-    return render_template('pages/wish-list.html', title='wish-list')
+def index():
+    return render_template('index.html')
